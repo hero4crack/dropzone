d2c3c4d9c5662eea83f015da8aaa2be1a6366811
hero4crack <hectorlamaquina14@gmail.com>
2025-11-22 23:16:45 -0400

    algo en admins

--- dropzone-contenido/admin/api/admins.php
+++ dropzone-contenido/admin/api/admins.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../config/database.php';
-require_once '../../includes/session.php';
+require_once '../../../config/database.php';
+require_once '../../../includes/session.php';
 
 header('Content-Type: application/json');
 
